a2a99d58ef0335f93e16aa47c951e97227c0f85d
achitchen <61246622+achitchen@users.noreply.github.com>
2022-10-11 22:46:38 +0100

    Detection overhaul
    Changed the detection system from raycast-only to being collision-based with a raycast to check line of sight

--- Assets/Scripts/DetectionScript.cs
+++ Assets/Scripts/DetectionScript.cs
@@ -4,19 +4,50 @@ using UnityEngine;
 
 public class DetectionScript : MonoBehaviour
 {
+    Vector3 lookDirForward;
+    Vector3 lookDirLeft;
+    Vector3 lookDirRight;
     Vector3 lookDir;
+    Collider alienCollider;
+    Vector3 alienColliderLeft;
+    Vector3 alienColliderRight;
+    public float lookAngle = 1.5f;
+
+
+    private void Start()
+    {
+        alienCollider = GetComponent<Collider>();
+    }
 
     private void FixedUpdate()
     {
-        CheckForPlayer();
+        //CheckForPlayer();
+        
     }
-
-    void CheckForPlayer()
+    private void OnTriggerEnter(Collider other)
     {
-        lookDir = transform.TransformDirection(Vector3.forward);
-        if (Physics.Raycast(transform.position, lookDir, 10)) 
+        if (other.gameObject.tag == "Player")
         {
-            print("I see you!");
+            RaycastHit hit;
+            lookDirForward = transform.TransformDirection(Vector3.forward);
+            lookDir = other.transform.position - this.transform.position;
+            Debug.DrawRay(transform.position, lookDir, Color.cyan, 10);
+            if (Physics.Raycast(transform.position, lookDir, out hit, 10))
+            {
+                Debug.Log(hit.collider.gameObject.name);
+                if (hit.collider)
+                {
+                    if (hit.transform.gameObject.tag == "Player")
+                    {
+                        Debug.Log("Found you!");
+                    }
+                    else
+                    {
+                        Debug.Log("Player is hidden");
+                    }
+                }
+
+            }
         }
     }
 }
