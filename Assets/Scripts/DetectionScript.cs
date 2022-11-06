38b301cd4bde676f62ae7117041e29fb56f28f8b
achitchen <61246622+achitchen@users.noreply.github.com>
2022-11-06 19:41:24 +0000

    Revamped stealth system
    Changed the way raycasts are processed and added a stealth timer to the detection logic. Player will now correctly be detected when moving out from cover

--- Assets/Scripts/DetectionScript.cs
+++ Assets/Scripts/DetectionScript.cs
@@ -5,49 +5,98 @@ using UnityEngine;
 public class DetectionScript : MonoBehaviour
 {
     Vector3 lookDirForward;
-    Vector3 lookDirLeft;
-    Vector3 lookDirRight;
     Vector3 lookDir;
-    Collider alienCollider;
-    Vector3 alienColliderLeft;
-    Vector3 alienColliderRight;
+    GameObject playerObject;
+    bool canDetect;
+    bool playerSeen;
     public float lookAngle = 1.5f;
+    [SerializeField] int detectionTimer = 0;
+    [SerializeField] int alertedTime = 100;
 
 
     private void Start()
     {
-        alienCollider = GetComponent<Collider>();
+        canDetect = false;
+        playerSeen = false;
     }
 
     private void FixedUpdate()
     {
-        //CheckForPlayer();
-        
+        if(canDetect && playerObject != null)
+        {
+            CheckForPlayer(playerObject);
+        }
+
+        if (playerSeen)
+        {
+            StopCoroutine(ResetAlarm());
+            if (detectionTimer < alertedTime)
+            {
+                detectionTimer += 1;
+            }
+            else
+            {
+                {
+                    Debug.Log("Kill player");
+                }
+                
+            }
+        }
+
+        if (!playerSeen && detectionTimer > 0)
+        {
+            StartCoroutine(ResetAlarm());
+        }
+
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-            RaycastHit hit;
-            lookDirForward = transform.TransformDirection(Vector3.forward);
-            lookDir = other.transform.position - this.transform.position;
-            Debug.DrawRay(transform.position, lookDir, Color.cyan, 10);
-            if (Physics.Raycast(transform.position, lookDir, out hit, 10))
-            {
-                Debug.Log(hit.collider.gameObject.name);
-                if (hit.collider)
-                {
-                    if (hit.transform.gameObject.tag == "Player")
-                    {
-                        Debug.Log("Found you!");
-                    }
-                    else
-                    {
-                        Debug.Log("Player is hidden");
-                    }
-                }
+            canDetect = true;
+            playerObject = other.gameObject;
+        }
+    }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            canDetect = false;
+            playerObject = null;
+        }
+    }
+
+    private void CheckForPlayer(GameObject other)
+    {
+        RaycastHit hit;
+        lookDirForward = transform.TransformDirection(Vector3.forward);
+        lookDir = other.transform.position - this.transform.position;
+        Debug.DrawRay(transform.position, lookDir, Color.cyan, 10);
+        if (Physics.Raycast(transform.position, lookDir, out hit, 10))
+        {
+            Debug.Log(hit.collider.gameObject.name);
+            if (hit.collider)
+            {
+                if (hit.transform.gameObject.tag == "Player")
+                {
+                    Debug.Log("Found you!");
+                    playerSeen = true;
+                }
+                else
+                {
+                    Debug.Log("Player is hidden");
+                    playerSeen = false;
+                }
             }
         }
     }
+
+    IEnumerator ResetAlarm()
+    {
+        yield return new WaitForSeconds(2f);
+        detectionTimer = 0;
+        playerSeen = false;
+    }
 }
+
